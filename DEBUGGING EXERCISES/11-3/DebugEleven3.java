37f4b55da9311e84e48e177554d44cd15b8fda74
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-12-05 13:37:47 -0600

    Add files via upload

---
+++ DEBUGGING EXERCISES/11-3/DebugEleven3.java
@@ -0,0 +1,11 @@
+public class DebugEleven3{
+public static void main(String args[]){
+DebugRowboat redBoat = new DebugRowboat();
+DebugRowboat blueBoat = new DebugRowboat();
+System.out.print("The two boats are");
+if(redBoat == blueBoat)
+System.out.println(" equal");
+else
+System.out.println(" not equal");
+}
+}
